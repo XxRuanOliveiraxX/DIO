8828c640f99b1a85e07ffc71522c2dc00b4b5c95
XxRuanOliveiraxX <162846693+XxRuanOliveiraxX@users.noreply.github.com>
2024-06-27 14:10:59 -0300

    1

---
+++ aula03.py
@@ -0,0 +1,14 @@
+numeros = list(range(2,10))
+print(numeros[-1]) #-1 pega o ultimo item da lista
+
+
+lista = list(input("digite algo: "))
+lista2 = ["p","y","t","h","o","n"]
+lista2[2] = "j"
+
+
+print(lista)
+print(lista2)
+print(lista[-1])
+print(lista2[:2])
+print(lista[::-1])
---
+++ aula04.py
@@ -0,0 +1,8 @@
+numeros = [14,7,8,12,20,6,61]
+pares = []
+
+for numero in numeros:
+    if numero % 2 ==0:
+        pares.append(numero)
+        
+print(pares)
